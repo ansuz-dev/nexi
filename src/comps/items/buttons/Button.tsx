f02db482061371d60eb4b8388cf61b66cb12442b
Ngu DUONG <nguduong1988@gmail.com>
2022-11-19 22:02:30 +0700

    update button

--- src/comps/items/buttons/Button.tsx
+++ src/comps/items/buttons/Button.tsx
@@ -2,21 +2,21 @@ import React from "react";
 import dynamic from "next/dynamic";
 import {GlobalButtonProps} from "./buttonprops";
 
-const Button001 = dynamic(() => import("./Button001"));
-
 const comps = {
-  "default": Button001,
-  "btn001": Button001,
+  "default": dynamic(() => import("./Button001"), {suspense: true}),
+  "btn001": dynamic(() => import("./Button001"), {suspense: true}),
 };
 
 const Button = ({layout, ...rest}: GlobalButtonProps) : JSX.Element => {
   const Component = comps[layout];
 
   return (
-    <Component {...rest} />
+    <React.Suspense fallback={"Loading..."}>
+      <Component {...rest} />
+    </React.Suspense>
   );
 };
 
-Button.defautProps = {layout: "default"};
+Button.defaultProps = {layout: "default"};
 
 export default Button;
