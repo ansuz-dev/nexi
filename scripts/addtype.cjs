7c454db6ddcb588115688b56fff4668ba004d23b
AnsuzDev <ansuzdev@gmail.com>
2023-03-09 23:21:53 +0700

    fix script

--- scripts/addtype.cjs
+++ scripts/addtype.cjs
@@ -5,7 +5,5 @@ console.log("Adding type:module to package.json");
 
 _package.type = "module";
 
-console.log(_package);
-
 // eslint-disable-next-line no-magic-numbers
 fs.writeFileSync("package.json", JSON.stringify(_package, null, 2));
